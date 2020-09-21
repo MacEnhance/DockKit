f5651307168666256111aad16757ce03ce8f4a39
Jeremy Legendre <jslegendre@gmail.com>
2020-09-21 15:56:53 -0400

    Turns out if I don't call dlclose, I don't have to hook [NSApplication load]

--- DockKit/DockKit.c
+++ DockKit/DockKit.c
@@ -9,23 +9,6 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <objc/objc-runtime.h>
 #include <dlfcn.h>
-#include "symrez.h"
-#include "subhook.h"
-
-extern int pthread_key_init_np(int key, void (*destructor)(void *));
-
- void NSApplication_load() {
-     symrez_t sr_appkit = symrez_new("AppKit");
-     if(!sr_appkit)
-         return;
-     
-     void *____NSAppKitFinalizeThreadData = sr_resolve_symbol(sr_appkit, "___NSAppKitFinalizeThreadData");
-     
-     if(!____NSAppKitFinalizeThreadData)
-         return;
-     
-     pthread_key_init_np(0x42, ____NSAppKitFinalizeThreadData);
- }
 
  __attribute__((constructor))
  static void ctor() {
@@ -37,22 +20,12 @@ extern int pthread_key_init_np(int key, void (*destructor)(void *));
                          NULL);
      
      // dlopen AppKit which will call [NSApplication load], which is where our problem lies
-     void *h = dlopen("/System/Library/Frameworks/AppKit.framework/Versions/C/AppKit", RTLD_LAZY);
+     dlopen("/System/Library/Frameworks/AppKit.framework/Versions/C/AppKit", RTLD_LAZY);
      
      // Replace the OG implementation because leaving that swizzle WILL cause more problems down the road
      class_replaceMethod(objc_getClass("NSBundle"),
                          sel_registerName("bundleIdentifier"),
                          ogBID,
                          NULL);
-     
-     // Grab a pointer to [NSApplication load]
-     Method origLoad = class_getClassMethod(objc_getClass("NSApplication"), sel_getUid("load"));
-     IMP origLoadImp = method_getImplementation(origLoad);
-     
-     // Replace -load with a rewrite of the funciton without the check for bundleIdentifier
-     subhook_t sh_nsAppLoad = subhook_new((void*)origLoadImp, NSApplication_load, SUBHOOK_64BIT_OFFSET);
-     subhook_install(sh_nsAppLoad);
-
-     dlclose(h);
  }
 
